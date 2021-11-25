e7b2df803f8f3eb4af3fa3b57adcc84dad305186
majster-pl <waliczek.szymon@gmail.com>
2021-11-25 23:29:43 +0000

    customise email header

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{URL('storage/email/email-header.png')}}" style="width:50%" alt="{{env('APP_NAME')}}">
+</a>
+</td>
+</tr>
