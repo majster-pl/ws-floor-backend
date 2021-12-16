2bae5ad46343003dbdff5f20328c632bb70b14e9
majster-pl <waliczek.szymon@gmail.com>
2021-12-16 23:17:48 +0000

    Code clean up

--- app/Http/Controllers/api/v1/AssetController.php
+++ app/Http/Controllers/api/v1/AssetController.php
@@ -15,35 +15,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AssetController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $assets = Asset::where("owner_id", Auth::user()->owner_id)->get()
             ->sortBy('reg');
         return new AssetCollection($assets);
-        // return $asset;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $data = $request->all();
@@ -56,12 +35,7 @@ class AssetController extends Controller
         return $new;
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
         $asset = Asset::where([['uuid', $id], ['owner_id', Auth::user()
@@ -77,24 +51,6 @@ class AssetController extends Controller
         }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $uuid)
     {
         $asset = Asset::where("uuid", $uuid)->first();
@@ -102,12 +58,6 @@ class AssetController extends Controller
         return new AssetResource($asset);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $asset = Asset::find($id)->delete();
