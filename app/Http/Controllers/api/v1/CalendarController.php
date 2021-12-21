3959b470b80927029f4c61f751e75cc4e5822429
majster-pl <waliczek.szymon@gmail.com>
2021-12-21 00:07:10 +0000

    Code clean up

--- app/Http/Controllers/api/v1/CalendarController.php
+++ app/Http/Controllers/api/v1/CalendarController.php
@@ -10,19 +10,15 @@ use App\Http\Resources\EventCollection;
 
 class CalendarController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    // * Display a listing of Events from date specified and number of days requested.
+    // * possible to pass foramt atribute to have data formated differently.
     public function index(Request $request)
     {
         $from = $request->from;
         if ($from === null) {
             return response()->json([
                 'error' => [
-                    'message' => 'unsupported request, please provide start date 
-                    [YYYY-MM-DD hh:mm] using "from" argument in request'
+                    'message' => 'unsupported request, please provide start date [YYYY-MM-DD hh:mm] using `from` argument in request'
                 ]
             ]);
         }
@@ -30,89 +26,18 @@ class CalendarController extends Controller
         if ($numberOfDays === null) {
             return response()->json([
                 'error' => [
-                    'message' => 'unsupported request, please provide number 
-                    of days [int] using "days" argument in request'
+                    'message' => 'unsupported request, please provide number of days [int] using `days` argument in request'
                 ]
             ]);
         }
         $depot = $request->depot;
 
         $to = date('Y-m-d h:m', strtotime(date($from) . ' + ' . $request->days . ' days'));
-        // $events = Event::withTrashed()->whereBetween('booked_date', [$from, $to])
-        //     ->orderBy('events.booked_date_time')
-        //     ->get();
         $events = Event::whereBetween('booked_date_time', [$from, $to])
             ->where([['owning_branch', $depot ? $depot : Auth::user()->default_branch]])
             ->orderBy('events.booked_date_time')
             ->get();
 
         return new EventCollection($events);
-    }
-
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
